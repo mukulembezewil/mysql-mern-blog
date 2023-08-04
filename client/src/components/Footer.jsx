ec81a50b0ec5590ff4c8bfaf432b5adebe08369a
mukulembezewil <wilmukulembeze@gmail.com>
2023-08-04 10:39:31 +0300

    Add the navbar and footer components

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../img/logo.png';
+
+const Footer = () => {
+	return (
+		<footer>
+			<img
+				src={Logo}
+				alt=""
+			/>
+			<span>&copy Mukulembeze Wilfred &copy 2020</span>
+		</footer>
+	);
+};
+
+export default Footer;
