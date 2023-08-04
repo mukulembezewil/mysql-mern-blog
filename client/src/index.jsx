d5a1013611c56e1200fa181e653c5000b16ac6f9
mukulembezewil <wilmukulembeze@gmail.com>
2023-08-04 12:11:45 +0300

    Add corrected AuthContextProvider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { AuthContextProvider } from './context/authContext.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<AuthContextProvider>
+			<App />
+		</AuthContextProvider>
+	</React.StrictMode>
+);
