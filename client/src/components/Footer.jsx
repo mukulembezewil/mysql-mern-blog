82298363b5e6fdee2b60c8ac46d4a20ad9d16cf5
mukulembezewil <wilmukulembeze@gmail.com>
2023-08-04 12:13:37 +0300

    Remove footer logo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,11 +4,7 @@ import Logo from '../img/logo.png';
 const Footer = () => {
 	return (
 		<footer>
-			<img
-				src={Logo}
-				alt=""
-			/>
-			<span>&copy Mukulembeze Wilfred &copy 2020</span>
+			<span>&copy; Mukulembeze Wilfred &copy; 2020</span>
 		</footer>
 	);
 };
