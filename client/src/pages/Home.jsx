c7b5438caf8ec3010b46f67c704fe67e196b52d6
mukulembezewil <wilmukulembeze@gmail.com>
2023-08-04 18:09:55 +0300

    Make home page work

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,48 +4,52 @@ import { Link, useLocation } from 'react-router-dom';
 import axios from 'axios';
 
 const Home = () => {
-	const [posts, setPosts] = useState([]);
+	// const [posts, setPosts] = useState([]);
 
-	const cat = useLocation().search;
+	// const cat = useLocation().search;
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const res = await axios.get(`/posts${cat}`);
-				setPosts(res.data);
-			} catch (err) {
-				console.log(err);
-			}
-		};
-		fetchData();
-	}, [cat]);
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		try {
+	// 			const res = await axios.get(`/posts${cat}`);
+	// 			setPosts(res.data);
+	// 		} catch (err) {
+	// 			console.log(err);
+	// 		}
+	// 	};
+	// 	fetchData();
+	// }, [cat]);
 
-	// const posts = [
-	//   {
-	//     id: 1,
-	//     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-	//     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-	//     img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-	//   },
-	//   {
-	//     id: 2,
-	//     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-	//     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-	//     img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-	//   },
-	//   {
-	//     id: 3,
-	//     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-	//     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-	//     img: "https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-	//   },
-	//   {
-	//     id: 4,
-	//     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-	//     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-	//     img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-	//   },
-	// ];
+	const posts = [
+		{
+			id: 1,
+			title: 'Understanding the power of mental vibration',
+			desc:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+		},
+		{
+			id: 2,
+			title: 'An introduction to the two essential laws of Thought',
+			desc:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+		},
+		{
+			id: 3,
+			title: 'Mental states and the several religious phenomena',
+			desc:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+		},
+		{
+			id: 4,
+			title: 'Mental faculties and the mental states',
+			desc:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+		},
+	];
 
 	const getText = (html) => {
 		const doc = new DOMParser().parseFromString(html, 'text/html');
