dfa70b900d6cd7034d18a7890769f46926418fd0
mukulembezewil <wilmukulembeze@gmail.com>
2023-08-09 17:24:40 +0300

    Edit single component

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,96 +1,63 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React from 'react';
 import Edit from '../img/edit.png';
 import Delete from '../img/delete.png';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Menu from '../components/Menu';
-import axios from 'axios';
-import moment from 'moment';
-import { AuthContext } from '../context/authContext';
-import DOMPurify from 'dompurify';
 
 const Single = () => {
-	const [post, setPost] = useState({});
-
-	const location = useLocation();
-	const navigate = useNavigate();
-
-	const postId = location.pathname.split('/')[2];
-
-	const { currentUser } = useContext(AuthContext);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const res = await axios.get(`/posts/${postId}`);
-				setPost(res.data);
-			} catch (err) {
-				console.log(err);
-			}
-		};
-		fetchData();
-	}, [postId]);
-
-	const handleDelete = async () => {
-		try {
-			await axios.delete(`/posts/${postId}`);
-			navigate('/');
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	const getText = (html) => {
-		const doc = new DOMParser().parseFromString(html, 'text/html');
-		return doc.body.textContent;
-	};
-
 	return (
 		<div className="single">
 			<div className="content">
-				<img
-					src={`../upload/${post?.img}`}
-					alt=""
-				/>
+				<img src="https://ie.auroville.org/content/images/2020/08/MentalFaculties.png"></img>
 				<div className="user">
-					{post.userImg && (
-						<img
-							src={post.userImg}
-							alt=""
-						/>
-					)}
+					<img src="https://ie.auroville.org/content/images/2020/08/MentalFaculties.png"></img>
 					<div className="info">
-						<span>{post.username}</span>
-						<p>Posted {moment(post.dated).fromNow()}</p>
+						<span>John</span>
+						<p>Posted 6 days ago</p>
 					</div>
-					{currentUser.username === post.username && (
-						<div className="edit">
-							<Link
-								to={`/write?edit=2`}
-								state={post}
-							>
-								<img
-									src={Edit}
-									alt=""
-								/>
-							</Link>
+					<div className="edit">
+						<Link to={'/write?edit=2'}>
 							<img
-								onClick={handleDelete}
-								src={Delete}
+								src={Edit}
 								alt=""
 							/>
-						</div>
-					)}
+						</Link>
+						<img
+							src={Delete}
+							alt=""
+						/>
+					</div>
 				</div>
-				<h1>{post.title}</h1>
-				<p
-					dangerouslySetInnerHTML={{
-						__html: DOMPurify.sanitize(post.desc),
-					}}
-				></p>{' '}
+				<h1>Understanding mental Gems</h1>
+				<p>
+					Psychological models of cognitive processing that take energy supply into
+					account remain uncommon. One exception is research on self-control
+					depletion, where debate continues over a limited-resource model. This model
+					argues that a transient reduction in self-control after the exertion of
+					prior self-control is caused by the depletion of brain glucose, and that
+					self-control processes are special, perhaps unique, in this regard.{' '}
+				</p>
+				<p>
+					This model has been argued to be physiologically implausible in several
+					recent reviews. This paper attempts to correct some inaccuracies that have
+					occurred during debate over the physiological plausibility of this model.
+					We contend that not only is such limitation of cognition by constraints on
+					glucose supply plausible, it is well established in the neuroscience
+					literature across several cognitive domains. Conversely, we argue that
+					there is no evidence that self-control is special in regard to its
+					metabolic cost.
+				</p>{' '}
+				<p>
+					Mental processes require physical energy, and the body is limited in its
+					ability to supply the brain with sufficient energy to fuel mental
+					processes. This article reviews current findings in brain metabolism and
+					seeks to resolve the current conflict in the field regarding the
+					physiological plausibility of the self-control glucose-depletion
+					hypothesis.
+				</p>
 			</div>
-			<Menu cat={post.cat} />
+			<Menu />
 		</div>
 	);
 };
-
 export default Single;
